f23cd0bef27bfb39d2ac12774387a01dff2208e3
meidadhf <meida.adhifa13@gmail.com>
2024-02-01 17:51:14 +0700

    Database-Migration

---
+++ database/seeders/PembayaransSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\str;
+
+class PembayaransSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $petugasIds = DB::table(table:'petugass')->pluck(column: 'id');
+        $sppIds = DB::table(table:'spps')->pluck(column: 'id');
+        DB::table('pembayarans')-> Insert([
+            'petugas_id' => $petugasIds->random(),
+            'nisn' => str::random(10),
+            'tgl_bayar' => now(),
+            'bulan_dibayar' => str::random(8),
+            'tahun_dibayar' => str::random(4),
+            'spp_id' => $sppIds->random(),
+            'jumlah_bayar' => random_int(0, 255),
+        ]);
+    }
+}
---
+++ resources/views/test.blade.php
@@ -0,0 +1,4 @@
+@extends('template.master');
+@section('header', 'ini halaman test');
+@section('content');
+@endsection
